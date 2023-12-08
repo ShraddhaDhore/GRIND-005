46991490caa474693b1651c5b29c45f8ee5310a0
ruchimi <ruchimi@W10PNTHNXTDG43.mavericsystems.lan>
2023-12-08 16:03:49 +0530

    tes demo added  by ruchi

--- src/package_1/MainClass.java
+++ src/package_1/MainClass.java
@@ -3,5 +3,6 @@ package package_1;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("I am in git demo");
+		
 	}
 }
---
+++ src/package_1/testDemo.java
@@ -0,0 +1,7 @@
+package package_1;
+
+public class testDemo {
+	System.out.println("I am in git demo");
+
+
+}
